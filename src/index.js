99598c4bec064de643643a8ff754a2d33c488cf1
Novia H <noviahoyte@gmail.com>
2024-10-29 01:05:01 -0400

    Added API integration and TypeWriter plugin.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 let formSubmit = document.querySelector("#submit-form");
-formSubmit.addEventListener("submit", generateSong);
+formSubmit.addEventListener("submit", generatePoem);
 
-function generateSong(event) {
+function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#song", {
-    strings: "Song lyrics will be inserted here once we integrate with AI API!",
+  let topicInput = document.querySelector("#poem-topic");
+  let prompt = `Generate a short rhyming poem about ${topicInput.value}`;
+  let context =
+    "Please generate a poem with a minimum of 4 lines and maximum of 6 lines about the given topic. Please structure your answer in basic HTML format: <p>answer</p>.";
+  let apiKey = "b00377005017b9aacft302b5od1aa426";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(prompt);
+  console.log(context);
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+function displayPoem(response) {
+  console.log(response.data);
+  new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
+    cursor: "",
   });
 }
