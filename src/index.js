73f2f768005b12a2426e23bc82869e78c79261a4
Novia H <noviahoyte@gmail.com>
2024-10-28 23:58:44 -0400

    Added event listener on form and function to handle form submit in index.js.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Inside of JS file!");
+let formSubmit = document.querySelector("#submit-form");
+formSubmit.addEventListener("submit", generateSong);
+
+function generateSong(event) {
+  event.preventDefault();
+
+  new Typewriter("#song", {
+    strings: "Song lyrics will be inserted here once we integrate with AI API!",
+    autoStart: true,
+  });
+}
