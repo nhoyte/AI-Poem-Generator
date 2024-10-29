8f129580dae1e9efc91d36256e28f7e4c1baa8a2
Novia H <noviahoyte@gmail.com>
2024-10-29 11:52:29 -0400

    CSS modifications.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function generatePoem(event) {
   let topicInput = document.querySelector("#poem-topic");
   let prompt = `Generate a short rhyming poem about ${topicInput.value}`;
   let context =
-    "Please generate a poem with a minimum of 4 lines and maximum of 6 lines about the given topic. Please structure your answer in basic HTML format: <p>answer</p>.";
+    "Please generate a poem with a minimum of 4 lines and maximum of 6 lines about the given topic. Please structure your answer in basic HTML format: <p>line 1</p> <p>line 2</p> <p>line 3</p> <p>line 4</p>.";
   let apiKey = "b00377005017b9aacft302b5od1aa426";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
@@ -19,6 +19,10 @@ function generatePoem(event) {
 
 function displayPoem(response) {
   console.log(response.data);
+  let title = "Generated Poem:";
+  let h3 = document.querySelector("#title");
+  h3.innerHTML = title;
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
